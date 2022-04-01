9726f62cdf3c6e88ce678e15832b1f37e3a80c54
Hiroki Yoshie <dymn9.term@gmail.com>
2022-04-01 14:34:58 +0900

    feat: read_from_stdin

--- src/main.c
+++ src/main.c
@@ -6,14 +6,50 @@
 /*   By: hyoshie <hyoshie@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/01 12:42:48 by hyoshie           #+#    #+#             */
-/*   Updated: 2022/04/01 12:51:30 by hyoshie          ###   ########.fr       */
+/*   Updated: 2022/04/01 14:34:38 by hyoshie          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "hotrace.h"
+#include "../inc/hotrace.h"
+#include "../libft/mylibft.h"
+
+
+void	read_stdin(t_list **kvl_lines, t_list **key_to_search)
+{
+	char	*line;
+
+	*kvl_lines = NULL;
+	while (get_next_line(STDIN_FILENO, &line) > 0 && *line != '\0')
+	{
+		ft_lstadd_back(kvl_lines, ft_lstnew(line));
+	}
+	*key_to_search = NULL;
+	while (get_next_line(STDIN_FILENO, &line) > 0)
+	{
+		ft_lstadd_back(key_to_search, ft_lstnew(line));
+	}
+}
+
+void	print_list(t_list *begin)
+{
+	while (begin != NULL)
+	{
+		ft_putendl_fd((char *)begin->content, 1);
+		begin  = begin->next;
+	}
+}
 
 int	main(void)
 {
+	t_list	*kvl_lines;
+	t_list	*key_to_search;
+	// t_dict	*kvl;
+
+	read_stdin(&kvl_lines, &key_to_search);
+	// kvl = store_kvl(key_to_search);
+	// print_value(kvl, key_to_search);
 	ft_putendl_fd("Hello!!", 1);
+	print_list(kvl_lines);
+	print_list(key_to_search);
 	return (0);
 }
